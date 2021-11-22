9ac4fa39287ee48f9313300c318aef7b73d97f78
Raimon Vilar <raimonv@gmail.com>
2021-11-22 19:24:01 +0100

    Control de versiones

---
+++ holaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,25 @@
+/*
+ * Proyecto holaMundoJava - Archivo HolaMundoJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * http://creativecommnos.org/licenses/by-nc-sa/4.0/
+ */
+
+package holamundojava;
+
+/**
+ *
+ * @author Raimon Vilar Morera <raimonv@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:05:57
+ */
+public class HolaMundoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO");
+    }
+
+}
